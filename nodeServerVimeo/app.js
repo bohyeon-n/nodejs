44fa5d2fdff9ced8e870f4e25f323405a72f19d4
koobohyeon <goosee115@gmail.com>
2019-07-14 19:33:41 +0900

    passport 환경 구축

--- nodeServerVimeo/app.js
+++ nodeServerVimeo/app.js
@@ -2,6 +2,10 @@ var express = require('express')
 var app = express()
 const bodyParser = require('body-parser')
 var router = require('./rotuer/index')
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const session = require('express-session')
+const flash = require('connect-flash')
 
 app.listen(3000, function() {
   console.log('start!! express server on port 3000')
